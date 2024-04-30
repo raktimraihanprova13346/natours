56be24cc85fde5c018121a43371d51dd0a5c97f5
Raktim Raihan Prova <67775189+raktimraihanprova13346@users.noreply.github.com>
2024-04-30 17:33:40 +0900

    Add Sass support and header component
    Sass support has been added to the project - package.json and package-lock.json have been updated accordingly. Additionally, a new header component, HeaderUI, has been created along with corresponding styling using Sass, and has been imported and used in App.js.

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./index.scss"
+import Logo from '../img/logo-white.png';
 
 const headerUI = () => {
     return(
         <div className="header">
-
+            <img src={Logo} alt="logo" className="header-logo" />
+            <div className="header-textbox">
+                <span className="header-textbox-first">Outdoors</span>
+                <span className="header-textbox-second">is where life happens</span>
+            </div>
         </div>
     )
 }
