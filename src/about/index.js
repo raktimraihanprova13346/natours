c16216b25a2f6e321f5cdd6ae299ad0f4283d650
Raktim Raihan Prova <raktim.raihan.prova@gmail.com>
2024-05-12 19:10:51 +0900

    Add 'header-button' styling and element in header
    This commit introduces styling for a new button in the header component, with different states for link, visited, hover, and active. It also includes adding the 'header-button' element with a link in the header component JSX file.

--- src/about/index.js
+++ src/about/index.js
@@ -6,12 +6,31 @@ const about = () => {
             <h2 className="about-title">
                 Exciting Tours for Adventurous People
             </h2>
-            <div className="message-01">
-                <h3 className="message-01-header">You are going to fall in Love in with</h3>
-                <p className="message-01-paragraph">
+            <div className="row">
+                <div className="col-01">
+                    <div className="message-01">
+                        <h3 className="message-01-header">You are going to fall in Love in with</h3>
+                        <p className="message-01-paragraph">
+                            You are going to fall in Love in withYou are going to fall in Love in with <br/> You are
+                            going to fall in Love in with <br/> You are going to fall in Love in with
+                        </p>
+                    </div>
 
-                </p>
+                    <div className="message-02">
+                        <h3 className="message-02-header">You are going to fall in Love in with</h3>
+                        <p className="message-02-paragraph">
+                            You are going to fall in Love in withYou are going to fall in Love in with <br/> You are
+                            going to fall in Love in with <br/> You are going to fall in Love in with
+                        </p>
+                    </div>
+
+                </div>
+                <div className="col-02">
+                    <p className="temp">Image Composition</p>
+                </div>
+
             </div>
+
         </div>
     )
 }
